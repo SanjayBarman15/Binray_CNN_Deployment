36e1180fd6b3a9b81843b3f082049f831e7a8608
SanjayBarman15 <sanjaybarman1179@gmail.com>
2025-12-09 15:46:42 +0530

    api fixed

--- backend/api.py
+++ backend/api.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from model_loader import predict_image
 
+import os
+
 app = FastAPI()
 
 # CORS for frontend
+origins = os.getenv("CORS_ORIGINS", "http://localhost:3000").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
